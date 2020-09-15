febb61701f37ba412dbc2604dda4968a103ac744
Roger Yong <rogeryonglucky@gmail.com>
2020-09-15 21:47:02 +0800

    机智题

---
+++ 算法/字符串/01串取反.py
@@ -0,0 +1,13 @@
+'''
+https://atcoder.jp/contests/arc088/tasks/arc088_b
+
+https://blog.csdn.net/algzjh/article/details/78900023
+
+题目大意：
+
+给出一个长度为10^5的01串，操作是每次选一个长度至少为K的区间，将01反转，可进行多次操作，要使得最后为全0，问K最大为多少。
+
+
+
+
+'''
