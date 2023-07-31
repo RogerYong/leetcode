560e9277aaf743a3ea9fbfb2e8d46177f740d551
RogerYong <rogeryonglucky@gmail.com>
2023-07-31 15:13:28 +0800

    JZ27

---
+++ 数据结构/树/二叉树/剑指offer27二叉树的镜像.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution
+{
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param pRoot TreeNode类 
+     * @return TreeNode类
+     */
+    TreeNode* Mirror(TreeNode* pRoot) {
+        // write code here
+        if(pRoot == nullptr){
+            return nullptr;
+        }
+        TreeNode * left = pRoot->left;
+        TreeNode* right = pRoot->right;
+
+        pRoot->left = Mirror(right);
+        pRoot->right = Mirror(left);
+        return pRoot;
+    }
+
+};
