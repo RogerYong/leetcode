57340e07e54e986438d049071d08c345d7207531
RogerYong <rogeryonglucky@gmail.com>
2023-08-11 21:17:10 +0800

    JZ76

--- 数据结构/线性表/链表/剑指offer76删除有序链表中重复的节点.java
+++ 数据结构/线性表/链表/剑指offer76删除有序链表中重复的节点.java
@@ -17,7 +17,7 @@ import java.util.*;
  * 如果用hash, 要遍历2次链表, 第一次计数, 第二次删除; 需要额外O(N)空间保存计数
  */
 public class Solution {
-    public ListNode deleteDuplication(ListNode pHead) {
+   public ListNode deleteDuplication(ListNode pHead) {
 
         ListNode fakeHead = new ListNode(-1);
         fakeHead.next = pHead;
@@ -27,21 +27,17 @@ public class Solution {
         // 重复节点的条件: 至少有两个连续节点, 节点值相等
         while (prevNode != null && prevNode.next != null &&
                 prevNode.next.next != null) {
-            // 第一步, 寻找prevNode后的重复节点值, 记录在repeatVal中, repeatVal==-1表示不存在重复节点
-            int repeatVal = -1;
+            // 寻找prevNode后的重复节点值
             if (prevNode.next.val == prevNode.next.next.val) {
-                repeatVal = prevNode.next.val;
-            }
-            if (repeatVal == -1) {
-                // 如果下一个节点是非重复节点, prevNode右移
-                prevNode = prevNode.next;
-            } else {
+                int repeatVal = prevNode.next.val;
                 // 删除重复节点
                 while (prevNode.next != null && prevNode.next.val == repeatVal) {
                     prevNode.next = prevNode.next.next;
                 }
+            } else {
+                // 下一个节点是非重复节点, prevNode右移
+                prevNode = prevNode.next;
             }
-
         }
         return fakeHead.next;
     }
