80c909d1a10e09c0d209b9dcecce198da9de861b
RogerYong <rogeryonglucky@gmail.com>
2023-08-16 00:30:33 +0800

    jz82

--- 数据结构/树/二叉树/剑指offer82二叉树中和为某一值的路径(一).java
+++ 数据结构/树/二叉树/剑指offer82二叉树中和为某一值的路径(一).java
@@ -1,3 +1,35 @@
-public class 剑指offer82二叉树中和为某一值的路径(一) {
-    
+import java.util.*;
+
+/*
+ * public class TreeNode {
+ *   int val = 0;
+ *   TreeNode left = null;
+ *   TreeNode right = null;
+ *   public TreeNode(int val) {
+ *     this.val = val;
+ *   }
+ * }
+ */
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param root TreeNode类
+     * @param sum int整型
+     * @return bool布尔型
+     */
+    public boolean hasPathSum (TreeNode root, int sum) {
+        // write code here
+        if (root == null) {
+            return false;
+        }
+        if (root.val == sum && root.left == null && root.right == null) {
+            return true;
+        } else {
+            return hasPathSum(root.left, sum - root.val) ||
+                   hasPathSum(root.right, sum - root.val) ;
+        }
+    }
 }
